a9a3fbf44b3101a4ccec49f405cac94fb217c35c
Mono <snayper00001@gmail.com>
2022-06-05 11:12:45 +0300

    Add log in page and start work on the user session

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_091429) do
+ActiveRecord::Schema.define(version: 2022_06_05_072838) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_06_04_091429) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
